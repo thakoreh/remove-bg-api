d71f032094b81d2b511f04b430337a4bda7e9cbe
Hiren Thakore <hiren.thakore58@gmail.com>
2022-09-01 18:03:11 -0400

    second commit

--- remove-bg.py
+++ remove-bg.py
@@ -21,8 +21,10 @@ def home():
 
 @app.post('/upload')
 def postImg():
-    with open(payload, 'rb') as i:
+    with open(data, 'rb') as i:
         with open(output_path, 'wb') as o:
             input = i.read()
             output = remove(input)
             o.write(output)
+
+# api_endpoint = "https://remove-bg-api.herokuapp.com/"
