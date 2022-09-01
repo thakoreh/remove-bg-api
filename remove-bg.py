16ff791f0e33a47131d784fe3bf2d8637719dd72
Hiren Thakore <hiren.thakore58@gmail.com>
2022-09-01 19:08:04 -0400

    new upload logic

--- remove-bg.py
+++ remove-bg.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI,Response,UploadFile
+from fastapi import FastAPI,Response,UploadFile,File
 import requests
 from fastapi.responses import FileResponse
 from rembg import remove
@@ -20,11 +20,20 @@ def home():
     return "Welcome to Home Page"
 
 @app.post('/upload')
-async def postImg(file:UploadFile):
-    with open(file.filename, 'rb') as i:
-        with open(output_path, 'wb') as o:
-            input = i.read()
-            output = remove(input)
-            o.write(output)
+async def postImg(file: UploadFile = File(...)):
+    if not file:
+        return {"message": "No file sent"}
+    else:
+        try:
+            contents = file.file.read()
+            # with open(file, 'rb') as i:
+            with open(output_path, 'wb') as o:
+                # input =contents.read()
+                output = remove(contents)
+                o.write(output)
+        except Exception:
+            return {"message": "There was an error uploading the file"}
+        finally:
+            file.file.close()
 
 # api_endpoint = "https://remove-bg-api.herokuapp.com/"
