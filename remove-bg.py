a6176d2576c3c8abec2e27aadcdb24adb5959b92
Hiren Thakore <hiren.thakore58@gmail.com>
2022-09-02 13:57:24 -0400

    working logic for file upload

--- remove-bg.py
+++ remove-bg.py
@@ -5,23 +5,15 @@ from rembg import remove
 
 app = FastAPI()
 
-
-# input_path = r'D:\mexico\20220506_111806.jpg'
 output_path = 'output.jpg'
 
-# with open(payload, 'rb') as i:
-#     with open(output_path, 'wb') as o:
-#         input = i.read()
-#         output = remove(input)
-#         o.write(output)
 
 @app.get('/')
 def home():
     return "Welcome to Home Page"
 
-@app.post('/upload')
-async def postImg(file: UploadFile = File(...)):
-    print(file)
+@app.post('/upload/')
+async def postImg(file: bytes = File()):
     if not file:
         return {"message": "No file sent"}
     else:
@@ -42,3 +34,4 @@ async def postImg(file: UploadFile = File(...)):
             return {"message": "There was an error uploading the file"}
 
 # api_endpoint = "https://remove-bg-api.herokuapp.com/"
+# "content-type": "multipart/form-data"
